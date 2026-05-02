97f487e40e36bbd51b9bdc7e23e172a6a3d2dfa3
vlcdck <pridatchenkovladislav@gmail.com>
2026-05-03 02:06:48 +0300

    Complete

--- src/main/java/com/scraper/scrapper/JobScrapperImpl.java
+++ src/main/java/com/scraper/scrapper/JobScrapperImpl.java
@@ -8,8 +8,6 @@ import com.scraper.repository.VacancyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
